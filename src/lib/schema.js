10628176cd8fe800d90d7a9afbe89d8cded7ed9e
Joshua <mwansakunda530@gmail.com>
2025-09-05 03:22:47 +0200

    Updated schemas

---
+++ src/lib/schema.js
@@ -0,0 +1,17 @@
+import { z } from "zod";
+
+//Register form schema
+export const RegisterFormSchema = z.object({
+  email: z.string().trim(),
+  password: z
+    .string()
+    .trim()
+    .min(8, { error: "Password must be atleast 8 characters" }),
+  confirmPassword: z.string().trim(),
+});
+
+//Login form schema
+export const LoginFormSchema = z.object({
+  email: z.string().trim(),
+  password: z.string().trim(),
+});
